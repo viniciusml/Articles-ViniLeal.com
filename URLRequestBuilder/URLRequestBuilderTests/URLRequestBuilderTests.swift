5714469520937b370281418121f8ffde3ab628db
Vinicius Leal <viniciusmoreiraleal@gmail.com>
2020-05-06 23:44:16 +0200

    'URLRequest.method()' sets correct method

--- URLRequestBuilder/URLRequestBuilderTests/URLRequestBuilderTests.swift
+++ URLRequestBuilder/URLRequestBuilderTests/URLRequestBuilderTests.swift
@@ -6,6 +6,16 @@
 //  Copyright © 2020 Vinicius Moreira Leal. All rights reserved.
 //
 
+// http://test.myAPI.com/anyEndpoint/?width=200&height=200
+// http://api.myAPI.com/anyEndpoint/?width=200&height=200
+
+//func desiredResult() {
+//    buildRequest(base: URL)
+//        .path(.endpoint)
+//        .query([queryItems])
+//        .method(.get)
+//}
+
 import XCTest
 
 protocol RequestBuilder {
@@ -19,6 +29,14 @@ class URLRequestBuilder {
     }
 }
 
+extension URLRequest {
+    func method(_ method: String) -> URLRequest {
+        var request = self
+        request.httpMethod = method
+        return request
+    }
+}
+
 class URLRequestBuilderTests: XCTestCase {
 
     func test_build_usesCorrectHostURL() {
@@ -28,5 +46,15 @@ class URLRequestBuilderTests: XCTestCase {
         let request = sut.build(from: url)
         
         XCTAssertEqual(request.url, url)
+    }
+    
+    func test_build_setsHTTPMethod() {
+        let sut = URLRequestBuilder()
+        let url = URL(string: "http://any-url.com")!
+        
+        let request = sut.build(from: url)
+                         .method("GET")
+        
+        XCTAssertEqual(request.httpMethod, "GET")
     }
 }
