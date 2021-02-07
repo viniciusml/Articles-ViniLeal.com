2b0e16edf8cf11451540c2a96ad258de7b7ff5eb
Vinicius Leal <viniciusmoreiraleal@gmail.com>
2021-02-07 21:14:44 +0000

    Observer adds product to queue

---
+++ InAppPurchase/InAppPurchase/StoreObserver.swift
@@ -0,0 +1,22 @@
+//
+//  StoreObserver.swift
+//  InAppPurchase
+//
+//  Created by Vinicius Moreira Leal on 07/02/2021.
+//
+
+import StoreKit
+
+public class StoreObserver {
+    
+    let queue: SKPaymentQueue
+    
+    public init(queue: SKPaymentQueue = .default()) {
+        self.queue = queue
+    }
+    
+    public func buy(_ product: SKProduct) {
+        let payment = SKMutablePayment(product: product)
+        queue.add(payment)
+    }
+}
--- InAppPurchase/InAppPurchaseTests/StoreObserverTests.swift
+++ InAppPurchase/InAppPurchaseTests/StoreObserverTests.swift
@@ -5,4 +5,39 @@
 //  Created by Vinicius Moreira Leal on 28/01/2021.
 //
 
-import Foundation
+import InAppPurchase
+import StoreKitTest
+import XCTest
+
+class StoreObserverTests: XCTestCase {
+    
+    func test_buy_addsPaymentRequestToQueue() {
+        let queue = PaymentQueueSpy()
+        let sut = StoreObserver(queue: queue)
+        
+        let product = TestProduct()
+        sut.buy(product)
+        
+        XCTAssertEqual(queue.messages, [.add])
+    }
+    
+    // MARK: Helpers
+    
+    private class PaymentQueueSpy: SKPaymentQueue {
+        enum Message {
+            case add
+        }
+        
+        private(set) var messages = [Message]()
+        
+        override func add(_ payment: SKPayment) {
+            messages.append(.add)
+        }
+    }
+    
+    private class TestProduct: SKProduct {
+        override var productIdentifier: String {
+            "test product"
+        }
+    }
+}
