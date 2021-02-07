4c0c1d8f498c2088ede1497dbd2a23ba86ee3701
Vinicius Leal <viniciusmoreiraleal@gmail.com>
2021-02-07 21:31:26 +0000

    Improve assertions by capturing product identifier

--- InAppPurchase/InAppPurchaseTests/StoreObserverTests.swift
+++ InAppPurchase/InAppPurchaseTests/StoreObserverTests.swift
@@ -3,10 +3,10 @@
 //  InAppPurchaseTests
 //
 //  Created by Vinicius Moreira Leal on 28/01/2021.
-//
+//  For end to end test: https://www.revenuecat.com/blog/storekit-testing-in-xcode
 
 import InAppPurchase
-import StoreKitTest
+import StoreKit
 import XCTest
 
 class StoreObserverTests: XCTestCase {
@@ -15,10 +15,11 @@ class StoreObserverTests: XCTestCase {
         let queue = PaymentQueueSpy()
         let sut = StoreObserver(queue: queue)
         
-        let product = TestProduct()
+        let product = TestProduct(identifier: "a product")
         sut.buy(product)
         
         XCTAssertEqual(queue.messages, [.add])
+        XCTAssertEqual(queue.addedProducts, ["a product"])
     }
     
     // MARK: Helpers
@@ -29,15 +30,24 @@ class StoreObserverTests: XCTestCase {
         }
         
         private(set) var messages = [Message]()
-        
+        private(set) var addedProducts = [String]()
+ 
         override func add(_ payment: SKPayment) {
             messages.append(.add)
+            addedProducts.append(payment.productIdentifier)
         }
     }
     
     private class TestProduct: SKProduct {
+        
+        let identifier: String
+        
+        init(identifier: String) {
+            self.identifier = identifier
+        }
+        
         override var productIdentifier: String {
-            "test product"
+            identifier
         }
     }
 }
