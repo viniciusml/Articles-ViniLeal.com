39a3797dd656d881e4b95ce471fce818a8f34a2e
Vinicius Leal <viniciusmoreiraleal@gmail.com>
2021-03-12 21:29:21 +0000

    Add Store Adapter

--- InAppPurchase/InAppPurchase/StoreLoader.swift
+++ InAppPurchase/InAppPurchase/StoreLoader.swift
@@ -7,6 +7,33 @@
 
 import StoreKit
 
+public protocol Request {
+    init(productIdentifiers: Set<String>)
+    func start()
+}
+
+extension SKProductsRequest: Request {}
+
+// We could use an abstract factory, or an Adapter, which can drastically simplify the design. With a factory, your code needs to ask an object to return something. With an adapter, you tell objects to do somthing.
+
+// The interface we want is 'fetchProducts(with identifiers: [String])'
+public class StoreLoaderAdapter {
+    private let factory: (Set<String>) -> Request
+    private let identifiers: [String]
+    
+    public init(identifiers: [String],
+                factory: @escaping (Set<String>) -> Request = SKProductsRequest.init(productIdentifiers:)) {
+        self.identifiers = identifiers
+        self.factory = factory
+    }
+    
+    public func createRequest() -> Request {
+        let productIdentifiers = Set(identifiers)
+        
+        return factory(productIdentifiers)
+    }
+}
+
 public class StoreLoader {
     let request: (Set<String>) -> SKProductsRequest
     
--- InAppPurchase/InAppPurchaseTests/StoreLoaderTests.swift
+++ InAppPurchase/InAppPurchaseTests/StoreLoaderTests.swift
@@ -9,28 +9,47 @@ import InAppPurchase
 import StoreKit
 import XCTest
 
-class StoreLoaderTests: XCTestCase {
+class StoreLoaderAdapterTests: XCTestCase {
     
-    func test_init_doesNotLoadProducts() {
-        let request = ProductsRequestSpy()
-        let sut = StoreLoader(request: { _ in
-            request
+    func test_adapter_createsProperRequest() {
+        let identifiers = ["id1", "id2"]
+        var requestSpy: ProductsRequestSpy!
+        let sut = StoreLoaderAdapter(identifiers: identifiers, factory: {
+            requestSpy = ProductsRequestSpy(productIdentifiers: $0)
+            return requestSpy
         })
         
-        XCTAssertTrue(request.messages.isEmpty)
+        _ = sut.createRequest()
+        
+        XCTAssertTrue(requestSpy.identifiers.isSubset(of: identifiers))
+    }
+}
+
+//class StoreLoaderTests: XCTestCase {
+//
+//    func test_init_doesNotLoadProducts() {
+//        let request = ProductsRequestSpy()
+//        let sut = StoreLoader(request: { _ in
+//            request
+//        })
+//
+//        XCTAssertTrue(request.messages.isEmpty)
+//    }
+//}
+
+class ProductsRequestSpy: Request {
+    enum Message {
+        case start
     }
     
-    // MARK: - Helpers
+    private(set) var messages = [Message]()
+    private(set) var identifiers = Set<String>()
     
-    private class ProductsRequestSpy: SKProductsRequest {
-        enum Message {
-            case start
-        }
-        
-        private(set) var messages = [Message]()
-        
-        override func start() {
-            messages.append(.start)
-        }
+    required init(productIdentifiers: Set<String> = Set()) {
+        identifiers.formUnion(productIdentifiers)
+    }
+    
+    func start() {
+        messages.append(.start)
     }
 }
