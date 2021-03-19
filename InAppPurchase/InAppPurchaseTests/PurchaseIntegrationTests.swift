b7da68557505c4b397af58d31dfcbd8056548a22
Vinicius Leal <viniciusmoreiraleal@gmail.com>
2021-03-19 23:42:15 +0000

    Setup integration test

--- InAppPurchase/InAppPurchaseTests/PurchaseIntegrationTests.swift
+++ InAppPurchase/InAppPurchaseTests/PurchaseIntegrationTests.swift
@@ -16,49 +16,34 @@ private enum Bakery {
 
 class PurchaseIntegrationTests: XCTestCase {
     
-//    func testInterruptedPurchase() throws {
-//        let session = try SKTestSession(configurationFileNamed: "RecipesAndCoins")
-//        session.resetToDefaultState()
-//        session.disableDialogs = true
-//
-//        session.interruptedPurchasesEnabled = true
-//        session.clearTransactions()
-//
-//        let identifier = Bakery.carrotCake
-//        let expectation = XCTestExpectation(description: "Wait for purchase")
-//
-//        let purchaseObserver = PaymentTransactionObserver()
-//        let purchaseLoader = ProductLoader(request: ProductRequestFactory.make(with: [identifier]))
-//
-//        purchaseLoader.fetchProducts()
-//        purchaseLoader.completion = { result in
-//            if let product = try? result.get().first {
-//
-//                purchaseObserver.buy(product)
-//                purchaseObserver.completion = { result in
-//
-//                }
-//            }
-//        }
-//
-//
-//        GreenBarContent.store.requestProductsAndBuy(
-//            productIdentifier: identifier
-//        ) { _ in
-//            let contentAvailable = GreenBarContent.store.receiptContains(identifier)
-//            let contentSaved = GreenBarContent.store.isProductPurchased(identifier)
-//
-//            XCTAssertFalse(
-//                contentAvailable,
-//                "Expected \(identifier) is not present in receipt")
-//            XCTAssertFalse(
-//                contentSaved,
-//                "Expected \(identifier) is not stored in PurchasedProducts")
-//
-//            expectation.fulfill()
-//        }
-//
-//        wait(for: [expectation], timeout: 60.0)
-//    }
+    func testPurchase() throws {
+        let session = try SKTestSession(configurationFileNamed: "TestSession")
+        session.resetToDefaultState()
+        session.disableDialogs = true
+
+        session.interruptedPurchasesEnabled = true
+        session.clearTransactions()
+
+        let identifier = Bakery.carrotCake
+        let expectation = XCTestExpectation(description: "Wait for purchase")
+
+        let purchaseObserver = PaymentTransactionObserver()
+        let purchaseLoader = ProductLoader(request: ProductRequestFactory.make(with: [identifier]))
+
+        purchaseLoader.fetchProducts()
+        purchaseLoader.completion = { result in
+            if let product = try? result.get().first {
+
+                purchaseObserver.buy(product)
+                purchaseObserver.completion = { transaction in
+                    
+                    XCTAssertEqual(transaction.identifier, identifier)
+                    expectation.fulfill()
+                }
+            }
+        }
+        
+        wait(for: [expectation], timeout: 0.5)
+    }
 }
 
