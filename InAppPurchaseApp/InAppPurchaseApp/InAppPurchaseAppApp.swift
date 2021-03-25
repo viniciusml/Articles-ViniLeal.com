d6c79f947e6bcfb8a0f102cc9b01ca28c52ae841
Vinicius Leal <viniciusmoreiraleal@gmail.com>
2021-03-25 20:58:12 +0000

    Create workspace, with integration IAP tests

---
+++ InAppPurchaseApp/InAppPurchaseApp/InAppPurchaseAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  InAppPurchaseAppApp.swift
+//  InAppPurchaseApp
+//
+//  Created by Vinicius Moreira Leal on 25/03/2021.
+//
+
+import SwiftUI
+
+@main
+struct InAppPurchaseAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
