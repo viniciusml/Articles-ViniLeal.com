21c96da3aef34e71b912a873ff71984373b0d1a0
Vinicius Leal <viniciusmoreiraleal@gmail.com>
2021-03-25 21:04:47 +0000

    Improve assertions

--- InAppPurchaseApp/InAppPurchaseIntegrationTests/PurchaseIntegrationTests.swift
+++ InAppPurchaseApp/InAppPurchaseIntegrationTests/PurchaseIntegrationTests.swift
@@ -9,11 +9,6 @@ import InAppPurchase
 import StoreKitTest
 import XCTest
 
-private enum Bakery {
-    public static let carrotCake = "com.inAppPurchase.carrot.cake"
-    public static let chocolateCake = "com.inAppPurchase.chocolate.cake"
-}
-
 class PurchaseIntegrationTests: XCTestCase {
     
     func testSuccessfullPurchase() throws {
@@ -24,11 +19,11 @@ class PurchaseIntegrationTests: XCTestCase {
 //        session.interruptedPurchasesEnabled = true
 //        session.clearTransactions()
 
-        let identifier = Bakery.carrotCake
+        let identifiers = expectedIdentifiers(.carrotCake, .chocolateCake)
         let expectation = XCTestExpectation(description: "Wait for purchase")
 
         let purchaseObserver = PaymentTransactionObserver()
-        let purchaseLoader = ProductLoader(request: ProductRequestFactory.make(with: [identifier]))
+        let purchaseLoader = ProductLoader(request: ProductRequestFactory.make(with: identifiers))
 
         purchaseLoader.fetchProducts()
         purchaseLoader.completion = { result in
@@ -39,7 +34,15 @@ class PurchaseIntegrationTests: XCTestCase {
                     purchaseObserver.buy(product)
                     purchaseObserver.completion = { transaction in
                         
-                        XCTAssertEqual(transaction.identifier, identifier)
+                        XCTAssertEqual(transaction.identifier, identifiers[0])
+                    }
+                }
+                
+                if let product = fetchedProducts.last {
+                    purchaseObserver.buy(product)
+                    purchaseObserver.completion = { transaction in
+                        
+                        XCTAssertEqual(transaction.identifier, identifiers[1])
                     }
                 }
             case let .failure(error):
@@ -50,6 +53,17 @@ class PurchaseIntegrationTests: XCTestCase {
         }
         
         wait(for: [expectation], timeout: 0.5)
+    }
+    
+    // MARK: - Helpers
+    
+    private func expectedIdentifiers(_ identifiers: Bakery...) -> [String] {
+        Array(identifiers.map { $0.rawValue })
+    }
+    
+    private enum Bakery: String {
+        case carrotCake = "com.inAppPurchase.carrot.cake"
+        case chocolateCake = "com.inAppPurchase.chocolate.cake"
     }
 }
 
